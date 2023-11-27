3c04e9c260bd90d1cf9a85a87d6a53079002e6ec
luishellobardo <luismartinhernandez89@gmail.com>
2023-11-27 15:06:15 -0300

    added useEffect and localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@
 2)RECIBO DEL FORMULARIO (TASKFORM) A TRAVES DE LA PROPIEDAD HANDLEADDITEM Y LA REENVIO
 3)AGREGO LA NUEVA TAREA AL HOOK Y LA REENVIO A LA LISTA DE TAREAS (TASKLIST) MEDIANTE LAS PROPIEDADES DEL HOOK
 */
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import './App.css';
 import { TaskForm } from './components/TaskForm';
 import { TaskList } from './components/TaskList';
 
+const KEY = 'listApp.items';
+
 export function App() {
   const [list, setList] = useState([]);
+
+  useEffect(() => {
+    let store = JSON.parse(localStorage.getItem(KEY));
+    if (store.length > 0) {
+      setList(store);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(list));
+  }, [list]);
+
   const handleAddItem = (newItem) => {
     setList([...list, newItem]);
   };
